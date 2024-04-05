a44ac44471b6d3b33219fc3db0d26136b9296549
Jeremi Nuer <jereminuerofficial@gmail.com>
2024-04-05 16:36:44 -0700

    Scraping URLs and creating dict with text

--- backend/data-pipeline/test_sources.py
+++ backend/data-pipeline/test_sources.py
@@ -1,18 +1,28 @@
 import json
 import getembeddings
+from articlescraper import ScrapedArticle
 with open("sorted_headlines.json", "r") as file:
     data = json.load(file)
 
-websites = [[]]
-
+scraped_articles = {}
+i = 1
 for key, items in data.items():
+    #only grabbing first 5 groups
+    if (i >= 6): break
     #group of articles that cover the same topic
     l1 = [key] + items
     group = []
     #We fetch ids for all the articles within this group
     fetch_response = getembeddings.index.fetch(ids=l1)
-    #We grab the URLs for each ID, and append to websites
+    #For each article URL, we scrape, and add to the text to the grouping
     for article in l1:
-        group.append(fetch_response['vectors'][article]['metadata']['url'])
-    websites.append(group)
+        url = fetch_response['vectors'][article]['metadata']['url']
+        source = fetch_response['vectors'][article]['metadata']['source']
+        article = ScrapedArticle(url, source)
+        group.append(article.to_text())
+
+    #We add entire group to our dict
+    scraped_articles[i] = group
+    i += 1
+
 
